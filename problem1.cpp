50c8eca5f7031c84a524e7d19830c863f4737434
scott-uni <s.anvarov@newuu.uz>
2026-01-29 10:07:36 +0500

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,18 @@
+#Problem1
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "+----------------------------------+\n";
+    cout << "|          STUDENT ID CARD          |\n";
+    cout << "+----------------------------------+\n";
+    cout << "| Name: Shomurod Anvarov           |\n";
+    cout << "| Student ID: 220056               |\n";
+    cout << "| Course: Computer Programming     |\n";
+    cout << "| Term: Spring 2026                |\n";
+    cout << "+----------------------------------+\n";
+
+    return 0;
+}
+
